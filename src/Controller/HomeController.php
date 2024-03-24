d03ae17b8a7acf364722c1414c9ef3664096e419
Damien Dupuy <sixty.nuuuts@gmail.com>
2024-03-24 08:02:39 +0100

    create security UserService for User|AnonymousUser

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\AnonymousUser;
+use App\Security\UserService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private User|AnonymousUser $user;
+
+    public function __construct(
+        private UserService $userService,
+    ) {
+        $this->user = $this->userService->getCurrentUser();
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        dd($this->user);
+
         return $this->render('home/index.html.twig');
     }
 }
---
+++ src/Security/UserService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use App\Entity\AnonymousUser;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class UserService
+{
+    public function __construct(
+        private Security $security,
+        private EntityManagerInterface $entityManager,
+        private RequestStack $requestStack,
+    ) {
+    }
+
+    public function getCurrentUser(): User|AnonymousUser
+    {
+        $user = $this->security->getUser();
+
+        // user is authenticated
+        if ($user instanceof User) {
+            return $user;
+        }
+
+        // else use the session to create or retrieve an anonymousUser
+        $session = $this->requestStack->getSession();
+
+        $sessionId = $session->getId();
+        if (!$sessionId) {
+            $session->set('anonymous_user_id', 'init');
+            $sessionId = $session->getId();
+        }
+
+        $anonymousUser = $this->entityManager->getRepository(AnonymousUser::class)->findOneBy(['sessionId' => $sessionId]);
+
+        if (!$anonymousUser) {
+            $anonymousUser = new AnonymousUser();
+            $anonymousUser->setSessionId($sessionId);
+
+            $this->entityManager->persist($anonymousUser);
+            $this->entityManager->flush();
+
+            $session->set('anonymous_user_id', $anonymousUser->getId());
+        }
+
+        return $anonymousUser;
+    }
+}
