233342ae6d874c9641ce3b987d88244fa49e7352
Damien Dupuy <sixty.nuuuts@gmail.com>
2024-06-18 22:55:36 +0200

    add css buttons

--- assets/navbar.js
+++ assets/navbar.js
@@ -2,20 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const burgerMenuBtn = document.getElementById('burgerMenuBtn');
     const burgerMenu = document.getElementById('burgerMenu');
     const menuOverlay = document.getElementById('menuOverlay');
+    const burgerMenuCross = document.querySelector('#burgerMenu > .cross');
 
     const accountMenuBtn = document.getElementById('accountMenuBtn');
     const accountDropdown = document.getElementById('accountDropdown');
 
     burgerMenuBtn.addEventListener('click', () => {
+        console.log('clicked');
         burgerMenuBtn.classList.toggle('open');
         burgerMenu.classList.toggle('open');
         menuOverlay.classList.toggle('open');
+        document.body.classList.toggle('overflow-hidden');
+    });
+
+    burgerMenuCross.addEventListener('click', () => {
+        burgerMenuBtn.classList.remove('open');
+        burgerMenu.classList.remove('open');
+        menuOverlay.classList.remove('open');
+        document.body.classList.remove('overflow-hidden');
     });
 
     menuOverlay.addEventListener('click', () => {
         burgerMenuBtn.classList.remove('open');
         burgerMenu.classList.remove('open');
         menuOverlay.classList.remove('open');
+        document.body.classList.remove('overflow-hidden');
     });
 
     accountMenuBtn.addEventListener('click', () => {
