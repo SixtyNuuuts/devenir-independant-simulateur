d1257ee65418b647e6e89911eac34e1ebdb43f84
Damien Dupuy <sixty.nuuuts@gmail.com>
2024-06-30 08:46:14 +0200

    ok

---
+++ assets/admin.js
@@ -0,0 +1,129 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('confirmDeleteModal');
+    const closeModal = modal.querySelector('.modal .cross');
+    const confirmDelete = document.getElementById('confirmDelete');
+    const cancelDelete = document.getElementById('cancelDelete');
+    let formToDelete, trToDelete;
+
+    document.querySelectorAll('.admin-delete-item').forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            formToDelete = form;
+            trToDelete = formToDelete.closest('tr');
+            modal.style.display = 'flex';
+        });
+    });
+
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    cancelDelete.addEventListener('click', () => {
+        modal.style.display = 'none';
+        formToDelete = null;
+    });
+
+    confirmDelete.addEventListener('click', () => {
+        if (formToDelete) {
+            const formData = new FormData(formToDelete);
+
+            console.log('Delete form:', formToDelete);
+
+            fetch(formToDelete.action, {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => {
+                    if (response.ok) {
+                        if (trToDelete) {
+                            trToDelete.classList.add('deleting');
+
+                            setTimeout(function () {
+                                trToDelete.remove();
+                            }, 500);
+                        }
+                    } else {
+                        return response.json().then(data => {
+                            console.error('Error:', data);
+                        });
+                    }
+                })
+                .catch(error => console.error('Fetch error:', error));
+        }
+        modal.style.display = 'none';
+        formToDelete = null;
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+            formToDelete = null;
+        }
+    });
+
+    const modalImportActivity = document.getElementById('overwriteConfirmImportActivityModal');
+    const closeImportActivityModal = modalImportActivity.querySelector('.modal .cross');
+    const confirmOverwrite = document.getElementById('confirmOverwrite');
+    const cancelOverwrite = document.getElementById('cancelOverwrite');
+    let formToImportActivity;
+
+
+    document.querySelectorAll('.admin-import-activity').forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            formToImportActivity = form;
+            submitForm(form);
+        });
+    });
+
+    closeImportActivityModal.addEventListener('click', () => {
+        modalImportActivity.style.display = 'none';
+    });
+
+    cancelOverwrite.addEventListener('click', () => {
+        modalImportActivity.style.display = 'none';
+        formToImportActivity = null;
+    });
+
+    confirmOverwrite.addEventListener('click', () => {
+        if (formToImportActivity) {
+            submitForm(formToImportActivity, true);
+        }
+        modalImportActivity.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modalImportActivity) {
+            modalImportActivity.style.display = 'none';
+            formToImportActivity = null;
+        }
+    });
+
+    function submitForm(form, overwrite = false) {
+        const formData = new FormData(form);
+        let actionUrl = form.action;
+
+        if (overwrite) {
+            actionUrl += '/overwrite';
+        }
+
+        fetch(actionUrl, {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => {
+                if (response.ok) {
+                    location.reload();
+                } else {
+                    if (response.status === 409) {
+                        modalImportActivity.style.display = 'flex';
+                    } else {
+                        return response.json().then(data => {
+                            console.error('Error:', data);
+                        });
+                    }
+                }
+            })
+            .catch(error => console.error('Fetch error:', error));
+    }
+});
--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 import { registerReactControllerComponents } from '@symfony/ux-react';
 import './bootstrap.js';
 import './navbar.js';
+import './admin.js';
 import './styles/app.scss';
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
